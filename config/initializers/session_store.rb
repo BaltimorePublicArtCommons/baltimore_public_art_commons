85702009b29cf959db2df336cfe167a8fd5bdcc7
Ali Ibrahim <aibrahim2k2@gmail.com>
2014-02-06 21:34:13 -0500

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BaltimorePublicArtCommons::Application.config.session_store :cookie_store, key: '_baltimore_public_art_commons_session'
