09329e4f6b50486ca27f62e1db65df3ad39fc90d
Ali Ibrahim <aibrahim2k2@gmail.com>
2014-02-15 14:52:27 -0500

    Update configuration settings
      * Generate new secret_key_base and load it through environment variable
      * Set enforce_available_locales to false since locales are not currently
        configured

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BaltimorePublicArtCommons::Application.config.secret_key_base = 'dff8d30d7211ba2c1d869d1e5dd4946f1e8c4005938307479b6d39f66b9178c9d1df04ef3fcce327061d172266f74a563be94fe74b00d94ae7d217ef26813f27'
+BaltimorePublicArtCommons::Application.config.secret_key_base = ENV['baltimore_public_works_secret_key_base']
