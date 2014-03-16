112bdf0dd6ee803bd5913765a114ea43b421ac35
David Raynes <rayners@gmail.com>
2014-03-16 16:21:56 -0400

    Importers modules!

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,49 +12,4 @@ class Item < ActiveRecord::Base
 
   has_attached_file :image, styles: { thumb: '100x100>' }
   validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
-
-  def self.import_from_open_baltimore!
-    image_base_url = 'http://data.baltimorecity.gov/views/5xsg-uc29/files/'
-    endpoint = 'http://data.baltimorecity.gov/resource/5xsg-uc29.json'
-
-    # row count is less than 1000, so we can just grab the whole thing
-    open(endpoint) do |j|
-      JSON.load(j.read)
-    end.each do |i|
-      item = Item.new(
-        name: i['titleofartwork'],
-        material: i['medium'],
-        installation_date: i['dateofartwork']
-      )
-      item.save!
-
-      if loc = i['location']
-        item.locations.create(longitude: loc['longitude'],
-                              latitude: loc['latitude'],
-                              current: true)
-      end
-
-      if i['artistfirstname'] || i['artistlastname']
-        artist = Artist.find_or_create_by(first_name: i['artistfirstname'], last_name: i['artistlastname'])
-        item.artists << artist
-      end
-
-      if i['image']
-        p i['image']
-        image_url = image_base_url + i['image']['file_id']
-        puts image_url
-        extname = File.extname(i['image']['filename'])
-        basename = File.basename(i['image']['filename'], extname)
-        Tempfile.create([basename, extname]) do |file|
-          file.binmode
-          open(image_url) do |f|
-            file.write f.read
-          end
-          file.rewind
-          item.image = file
-          item.save!
-        end
-      end
-    end
-  end
 end
