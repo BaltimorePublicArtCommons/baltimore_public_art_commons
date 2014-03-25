f2cf6765743f9691a35cf7e67b918d17c7a88d18
David Raynes <rayners@gmail.com>
2014-03-25 19:56:26 -0400

    Finished cleaning up migrations

--- db/migrate/20140226001547_create_artists_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateArtistsItems < ActiveRecord::Migration
-  def change
-    create_table :artists_items do |t|
-      t.belongs_to :artist
-      t.belongs_to :item
-      t.string :contribution, null: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -52,10 +52,10 @@ ActiveRecord::Schema.define(version: 20140319014003) do
   end
 
   create_table "images", force: true do |t|
+    t.string   "file"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "item_id"
-    t.string   "file"
   end
 
   create_table "individual_donors", force: true do |t|
@@ -87,10 +87,6 @@ ActiveRecord::Schema.define(version: 20140319014003) do
     t.string   "installation_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "locations", force: true do |t|
