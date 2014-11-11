44f4814f84f8f7792342a01924719d21a88f3881
Natasha Jones <tasha.k.jones@gmail.com>
2014-11-11 14:20:32 -0500

    replace login notice with secondary header * added sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      redirect_to root_url, notice: ("Logged in as " + user.first_name)
+      log_in user
+      redirect_to root_url
     else
       flash.alert = 'The credentials you entered are invalid. Please try again.'
       render :new
