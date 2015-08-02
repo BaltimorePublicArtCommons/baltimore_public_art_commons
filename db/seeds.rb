b52da52b6b16c64ec89b61c9dbf8d48c5df659a2
Polly Sileo <pollysileo@gmail.com>
2015-08-02 13:03:10 -0400

    Add first_name and last_name to seed admin
    [#100353856]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 Importers::Csv.import('db/seeds/sample_data.csv', 'db/seeds/images')
 
-User.create(email: "admin@publicartcommons.dev", password: "password", role: 0)
+User.create(first_name: "Hans", last_name: "Schuler" email: "admin@publicartcommons.dev", password: "password", role: 0)
