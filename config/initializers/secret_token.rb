5e95eb08c58546a7479832049b1a7c41dfab497c
Polly Sileo <pollysileo@gmail.com>
2015-08-02 12:15:21 -0400

    Take out check for user being logged in and removed session secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,4 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 BaltimorePublicArtCommons::Application.config.secret_key_base =
-  '84f80b26c026eb73a1a22ebc289603d4b032b50bbabb534b6e5e40d1845e98dd653c52d1cfec48e88be36078967961432a85c3eba3a412c136f26b893edbd5a1'
+  ENV['bpac_secret_key_base']
